37f487d643af1418e6c90d1cdc0937112a25f724
arthur <arthur@arthur-ra0xx.home>
2024-12-19 11:28:02 +0200

    editing the sign-up page

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -29,12 +29,12 @@ export default function Page() {
         </a>
 
         <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-          Welcome to Squid 🦑
+          Welcome to CTRL S
         </h2>
 
         <p className="mt-4 leading-relaxed text-white/90">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-          quibusdam aperiam voluptatum.
+        Track, analyze, and optimize your finances seamlessly. 
+        Control your money, your goals, and your future with confidence.
         </p>
       </div>
     </section>
@@ -63,12 +63,12 @@ export default function Page() {
           </a>
 
           <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-            Welcome to Squid 🦑
+            Welcome to CTRL S
           </h1>
 
           <p className="mt-4 leading-relaxed text-gray-500">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-            quibusdam aperiam voluptatum.
+          Track, analyze, and optimize your finances seamlessly. 
+          Control your money, your goals, and your future with confidence.
           </p>
         </div>
 
